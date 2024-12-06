083964fcca2864aba3f532fd53e40e9c7c36fd5a
ESMA-JULIEN-H <julienherzog@orange.fr>
2024-12-06 15:55:43 +0100

    06_12_2024_001

--- Assets/Scenes/Script/PressApple.cs
+++ Assets/Scenes/Script/PressApple.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
-using System.Collections;
-using System.Collections.Generic;
 
 public class ButtonResizer : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
@@ -11,19 +9,69 @@ public class ButtonResizer : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     private Vector3 originalScale;
     private Vector3 enlargedScale;
     public TextMeshProUGUI monTextUI;
+    public TextMeshProUGUI bananeUI;
 
     public float Money;
     public float TotalMoney = 0;
 
+    public int BananeTotal =0;
+    public int banane = 0;
+    public Button bananeToClick;
+    // Bouton à cliquer pour augmenter l'argent
+    public Button buttonToClick;
+
     // Facteur d'agrandissement (10% de plus)
     [SerializeField]
     private float scaleFactor = 1.1f;
+    public void CallBanane()
+    {
+        if (Money >= 20)
+        {
+            Money -= 20;
+            print("banane");
+            BananeTotal = BananeTotal + banane;
+            BananeTotal++;
+            bananeUI.text = BananeTotal.ToString("0");
+        }
+    }
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         originalScale = rectTransform.localScale;
         enlargedScale = originalScale * scaleFactor;
+    }
+
+    private void Start()
+    {
+        Money = TotalMoney;
+
+        // Ajouter un listener pour l'événement OnClick du bouton
+        if (buttonToClick != null)
+        {
+            buttonToClick.onClick.AddListener(AddMoney);
+        }
+    }
+    void Update()
+    {
+        if (buttonToClick != null && Money >= 20)
+        {
+            CallBanane();
+            print("oui");
+        }
+        if (buttonToClick != null && Money < 20) {
+
+            print("non");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Retirer le listener pour éviter les erreurs si l'objet est détruit
+        if (buttonToClick != null)
+        {
+            buttonToClick.onClick.RemoveListener(AddMoney);
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -35,18 +83,11 @@ public class ButtonResizer : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     {
         rectTransform.localScale = originalScale; // Retourne à la taille originale
     }
-    void Start()
-    {
-        Money = TotalMoney;
 
-
-    }
-    void Update()
+    // Méthode pour ajouter de l'argent
+    private void AddMoney()
     {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            Money++;
-        }
+        Money++;
         monTextUI.text = Money.ToString("0");
     }
 }
